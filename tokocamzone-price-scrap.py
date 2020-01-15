15ffbd2a35a62bc955022d6cffbfc2b6beb16340
jsoeryana <59072556+jsoeryana@users.noreply.github.com>
2020-01-15 08:30:28 +0800

    Add comments

--- tokocamzone-price-scrap.py
+++ tokocamzone-price-scrap.py
@@ -1,3 +1,5 @@
+#These codes are written with Python 3
+
 import bs4
 import sys
 from datetime import datetime
@@ -8,21 +10,26 @@ import requests
 #change the URL below as necessary
 target_url = "https://www.tokocamzone.com/kamera/Kamera-DSLR?limit=100"
 
+#get the target page based on the above URL
 uClient = requests.get(target_url, timeout=5)
 print("opening ... " + uClient.request.url)
+
+#store the html content into beatifulsoup object
 page_soup = BeautifulSoup(uClient.content,"html.parser")
 uClient.close()
 
+#find all the html tags which contains the target data to parse
 containers = page_soup.findAll("div",{"class":"caption"})
 
 print("Item found = " + str(len(containers)))
 
-
+#declare CSV file as output
 out_filename = str(datetime.now()) + ".csv"
 out_fileheader = "Article Name;Price\n"
 csv_file = open(out_filename,"w")
 csv_file.write(out_fileheader)
 
+#parse and put the results into csv file
 try:
     i = 1
     for container in containers:
